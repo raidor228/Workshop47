c37537271d3aaba4e8b3afcc2a63c759b11772d4
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-21 15:51:32 +0300

    Create config scriptable objects with settings

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/ApplicationSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Workshop47.Scripts.Game.Settings
+{
+    [CreateAssetMenu(fileName = "ApplicationSettings", menuName = "Game Settings/New Application Settings")]
+    public class ApplicationSettings : ScriptableObject
+    {
+        public int MusicVolume;
+        public int SFXVolume;
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Workshop47.Scripts.Game.Settings
+{
+    [CreateAssetMenu(fileName = "GameSettings", menuName = "Game Settings/New Game Settings")]
+    public class GameSettings : ScriptableObject
+    {
+
+    }
+}
