219e6dc15cb29d8b09eee75da08dbcda7b8154be
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-21 14:05:32 +0300

    Create GameState

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/GameState.cs
@@ -0,0 +1,17 @@
+﻿namespace Workshop47.Scripts.Game.State
+{
+    public class GameState
+    {
+        private readonly GameStateData _gameStateData;
+
+        public GameState(GameStateData gameStateData)
+        {
+            _gameStateData = gameStateData;
+        }
+
+        public int CreateEntityId()
+        {
+            return _gameStateData.CreateEntityId();
+        }
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/GameStateData.cs
@@ -0,0 +1,12 @@
+﻿namespace Workshop47.Scripts.Game.State
+{
+    public class GameStateData
+    {
+        public int GlobalEntityId { get; set; }
+
+        public int CreateEntityId()
+        {
+            return GlobalEntityId++;
+        }
+    }
+}
