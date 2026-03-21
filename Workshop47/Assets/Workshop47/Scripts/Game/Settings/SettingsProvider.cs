98bb3de8658a382479abbf150ff892489bd434f3
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-21 15:52:12 +0300

    Create settings provider

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/SettingsProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Workshop47.Scripts.Game.Settings
+{
+    public class SettingsProvider : ISettingsProvider
+    {
+        public GameSettings GameSettings => _gameSettings;
+        public ApplicationSettings ApplicationSettings { get; }
+
+        private GameSettings _gameSettings;
+        
+        public SettingsProvider()
+        {
+            ApplicationSettings = Resources.Load<ApplicationSettings>("ApplicationSettings");
+        }
+        
+        public Task<GameSettings> LoadGameSettings()
+        {
+            _gameSettings = Resources.Load<GameSettings>("GameSettings");
+            
+            return Task.FromResult(_gameSettings);
+        }
+    }
+}
