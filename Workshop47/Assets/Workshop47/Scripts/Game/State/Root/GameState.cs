b6d0ef18267cb53596a50c2bc1583e1e3cc4d03d
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-21 18:01:06 +0300

    Update game state data

--- Workshop47/Assets/Workshop47/Scripts/Game/State/Root/GameState.cs
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Root/GameState.cs
@@ -1,17 +1,44 @@
-﻿namespace Workshop47.Scripts.Game.State
+﻿using System.Linq;
+using ObservableCollections;
+using R3;
+using Workshop47.Scripts.Game.State.GameResources;
+
+namespace Workshop47.Scripts.Game.State
 {
     public class GameState
     {
+        public ObservableList<Resource> Resources { get; } = new();
+
         private readonly GameStateData _gameStateData;
 
         public GameState(GameStateData gameStateData)
         {
             _gameStateData = gameStateData;
+            
+            InitResources(gameStateData);
         }
 
         public int CreateEntityId()
         {
             return _gameStateData.CreateEntityId();
+        }
+        
+        private void InitResources(GameStateData gameStateData)
+        {
+            gameStateData.Resources.ForEach(resourceData => Resources.Add(new Resource(resourceData)));
+            
+            Resources.ObserveAdd().Subscribe(e =>
+            {
+                var addedResource = e.Value;
+                gameStateData.Resources.Add(addedResource.Origin);
+            });
+            
+            Resources.ObserveRemove().Subscribe(e =>
+            {
+                var removedResource = e.Value;
+                var removedResourceData = gameStateData.Resources.FirstOrDefault(b => b.ResourceType == removedResource.ResourceType);
+                gameStateData.Resources.Remove(removedResourceData);
+            });
         }
     }
 }
--- Workshop47/Assets/Workshop47/Scripts/Game/State/Root/GameStateData.cs
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Root/GameStateData.cs
@@ -1,8 +1,12 @@
-﻿namespace Workshop47.Scripts.Game.State
+﻿using System.Collections.Generic;
+using Workshop47.Scripts.Game.State.GameResources;
+
+namespace Workshop47.Scripts.Game.State
 {
     public class GameStateData
     {
         public int GlobalEntityId { get; set; }
+        public List<ResourceData> Resources { get; set; }
 
         public int CreateEntityId()
         {
