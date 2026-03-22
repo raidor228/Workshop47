fe227ce3a0cf2a1ac7b8ef50b13439ff091cbafa
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 16:45:19 +0300

    Create PlaceEntity command

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/CmdPlaceEntity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Workshop47.Scripts.Game.State.Commands;
+using Workshop47.Scripts.Game.State.Entities;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands
+{
+    public class CmdPlaceEntity : ICommand
+    {
+        public readonly EntityType EntityType;
+        public readonly string EntityConfigId;
+        public readonly Vector3 Position;
+        public readonly Vector3 Rotation;
+        
+        public CmdPlaceEntity(EntityType entityType, string entityConfigId, Vector3 position, Vector3 rotation)
+        {
+            EntityType = entityType;
+            EntityConfigId = entityConfigId;
+            Position = position;
+            Rotation = rotation;
+        }
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/Handlers/CmdPlaceEntityHandler.cs
@@ -0,0 +1,39 @@
+﻿using Workshop47.Scripts.Game.State.Commands;
+using Workshop47.Scripts.Game.State.Entities;
+using Workshop47.Scripts.Game.State.Entities.Upgradeable.Characters;
+using Workshop47.Scripts.Game.State.Root;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands.Handlers
+{
+    public class CmdPlaceEntityHandler : ICommandHandler<CmdPlaceEntity>
+    {
+        private readonly GameState _gameState;
+
+        public CmdPlaceEntityHandler(GameState gameState)
+        {
+            _gameState = gameState;
+        }
+        
+        public bool Handle(CmdPlaceEntity command)
+        {
+            var entityConfigId = command.EntityConfigId;
+            var entityType = command.EntityType;
+            var entityPosition = command.Position;
+            var entityRotation = command.Rotation;
+            var entityId = _gameState.CreateEntityId();
+            var createdEntityData = entityType switch
+            {
+                EntityType.Character => EntitiesDataFactory.CreateEntity<CharacterEntityData>(
+                    entityType, entityConfigId,entityPosition, entityRotation),
+                _ => throw new System.NotImplementedException(),
+            };
+
+            createdEntityData.UniqueId = entityId;
+            var createEntity = EntitiesFactory.CreateEntity(createdEntityData);
+            
+            _gameState.Entities.Add(createEntity);
+
+            return true;
+        }
+    }
+}
