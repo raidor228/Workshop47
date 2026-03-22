19a324610a044c8835f23a28ffee34ea6159df87
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 17:33:40 +0300

    Create map state

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Maps/Map.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using ObservableCollections;
+using Workshop47.Scripts.Game.State.Entities;
+using R3;
+
+namespace Workshop47.Scripts.Game.State.Maps
+{
+    public class Map
+    {
+        public MapData Origin { get; }
+        
+        public int Id => Origin.Id;
+        
+        public ObservableList<Entity> Entities { get; } = new();
+
+        public Map(MapData mapData)
+        {
+            Origin = mapData;
+            
+            mapData.Entities.ForEach(entityData => Entities.Add(EntitiesFactory.CreateEntity(entityData)));
+            
+            Entities.ObserveAdd().Subscribe(e =>
+            {
+                var addedEntity = e.Value;
+                mapData.Entities.Add(addedEntity.Origin);
+            });
+            
+            Entities.ObserveRemove().Subscribe(e =>
+            {
+                var removedEntity = e.Value;
+                var removedEntityData = mapData.Entities.FirstOrDefault(b => b.UniqueId == removedEntity.UniqueId);
+                mapData.Entities.Remove(removedEntityData);
+            });
+        }
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Maps/MapData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Workshop47.Scripts.Game.State.Entities;
+
+namespace Workshop47.Scripts.Game.State.Maps
+{
+    public class MapData
+    {
+        public int Id { get; set; }
+        public List<EntityData> Entities { get; set; }
+    }
+}
