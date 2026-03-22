2983427a6c8500d6ca25332c48d9da962993cd45
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 17:38:49 +0300

    Create map commands

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/CmdCreateMap.cs
@@ -0,0 +1,14 @@
+﻿using Workshop47.Scripts.Game.State.Commands;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands
+{
+    public class CmdCreateMap: ICommand
+    {
+        public readonly int MapId;
+        
+        public CmdCreateMap(int mapId)
+        {
+            MapId = mapId;
+        }
+    }
+}
