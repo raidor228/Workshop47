901ca39bf8ebd004892ef7abbe0c622bc1e7563d
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 17:27:15 +0300

    Add Characters to settlement world

--- Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Root/View/WorldSettlementRootBinder.cs
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Root/View/WorldSettlementRootBinder.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using ObservableCollections;
+using UnityEngine;
+using Workshop47.Scripts.Game.Settlement.View.Characters;
 using R3;
 
 namespace Workshop47.Scripts.Game.Settlement.Root.View
 {
     public class WorldSettlementRootBinder : MonoBehaviour
     {
+        private readonly Dictionary<int, CharacterBinder> _createdCharactersMap = new();
+        
         private readonly CompositeDisposable _disposables = new();
 
         private WorldSettlementRootViewModel _viewModel;
@@ -12,8 +17,41 @@ namespace Workshop47.Scripts.Game.Settlement.Root.View
         public void Bind(WorldSettlementRootViewModel viewModel)
         {
             _viewModel = viewModel;
+            
+            foreach (var buildingViewModel in viewModel.AllCharacters)
+            {
+                CreateBuilding(buildingViewModel);
+            }
+            
+            _disposables.Add(viewModel.AllCharacters.ObserveAdd()
+                .Subscribe(e => CreateBuilding(e.Value)));
+            
+            _disposables.Add(viewModel.AllCharacters.ObserveRemove()
+                .Subscribe(e => DestroyBuilding(e.Value)));
         }
 
+        private void CreateBuilding(CharacterViewModel characterViewModel)
+        {
+            var characterLevel = characterViewModel.Level.CurrentValue;
+            var characterType = characterViewModel.ConfigId;
+            var prefabCharacterLevelPath = $"Prefabs/Settlement/Characters/Character_{characterType}_{characterLevel}";
+            var characterBinder = Resources.Load<CharacterBinder>(prefabCharacterLevelPath);
+            var createdCharacter = Instantiate(characterBinder);
+            
+            createdCharacter.Bind(characterViewModel);
+
+            _createdCharactersMap[characterViewModel.EntityId] = createdCharacter;
+        }
+
+        private void DestroyBuilding(CharacterViewModel characterViewModel)
+        {
+            if (_createdCharactersMap.TryGetValue(characterViewModel.EntityId, out var characterBinder))
+            {
+                Destroy(characterBinder.gameObject);
+                _createdCharactersMap.Remove(characterViewModel.EntityId);
+            }
+        }
+        
         private void OnDestroy()
         {
             _disposables.Dispose();
--- Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Root/View/WorldSettlementRootViewModel.cs
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Root/View/WorldSettlementRootViewModel.cs
@@ -1,9 +1,16 @@
-﻿namespace Workshop47.Scripts.Game.Settlement.Root.View
+﻿using ObservableCollections;
+using Workshop47.Scripts.Game.Settlement.Services;
+using Workshop47.Scripts.Game.Settlement.View.Characters;
+
+namespace Workshop47.Scripts.Game.Settlement.Root.View
 {
     public class WorldSettlementRootViewModel
     {
-        public WorldSettlementRootViewModel()
+        public readonly IObservableCollection<CharacterViewModel> AllCharacters;
+        
+        public WorldSettlementRootViewModel(CharactersService charactersService)
         {
+            AllCharacters = charactersService.AllCharacters;
         }
     }
 }
