666b260e1e6890783737a3c3b2edcb6943136420
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 17:03:49 +0300

    Add EntitiesSettings to GameSettings

--- Workshop47/Assets/Workshop47/Scripts/Game/Settings/GameSettings.cs
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/GameSettings.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
+using Workshop47.Scripts.Game.Settings.Settlement.Entities;
 
 namespace Workshop47.Scripts.Game.Settings
 {
     [CreateAssetMenu(fileName = "GameSettings", menuName = "Game Settings/New Game Settings")]
     public class GameSettings : ScriptableObject
     {
-
+        public EntitiesSettings EntitiesSettings;
     }
 }
