543d01b2480dacc4bf25d5762fe180964673c1ea
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 15:59:13 +0300

    Create character entity data

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Entities/Characters/CharacterEntity.cs
@@ -0,0 +1,21 @@
+﻿using R3;
+
+namespace Workshop47.Scripts.Game.State.Entities.Characters
+{
+    public class CharacterEntity : Entity
+    {
+        public string Name => _origin.Name;
+        
+        public readonly ReactiveProperty<int> Level;
+        
+        private readonly CharacterEntityData _origin;
+        
+        public CharacterEntity(CharacterEntityData data) : base(data)
+        {
+            _origin = data;
+
+            Level = new ReactiveProperty<int>(data.Level);
+            Level.Skip(1).Subscribe(newLevel => data.Level = newLevel);
+        }
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Entities/Characters/CharacterEntityData.cs
@@ -0,0 +1,8 @@
+﻿namespace Workshop47.Scripts.Game.State.Entities.Characters
+{
+    public class CharacterEntityData : EntityData
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
