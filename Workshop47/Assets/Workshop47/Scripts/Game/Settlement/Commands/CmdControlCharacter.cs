5a6363a9a926622e9a700f68cc8479f3c5e736bf
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 20:18:49 +0300

    Create control character command

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/CmdControlCharacter.cs
@@ -0,0 +1,14 @@
+﻿using Workshop47.Scripts.Game.State.Commands;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands
+{
+    public class CmdControlCharacter : ICommand
+    {
+        public readonly int CharacterId;
+
+        public CmdControlCharacter(int characterId)
+        {
+            CharacterId = characterId;
+        }
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/Handlers/CmdControlCharacterHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using UnityEngine;
+using Workshop47.Scripts.Game.State.Commands;
+using Workshop47.Scripts.Game.State.Root;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands.Handlers
+{
+    public class CmdControlCharacterHandler : ICommandHandler<CmdControlCharacter>
+    {
+        private readonly GameState _gameState;
+
+        public CmdControlCharacterHandler(GameState gameState)
+        {
+            _gameState = gameState;
+        }
+        
+        public bool Handle(CmdControlCharacter command)
+        {
+            var currentMap = _gameState.Maps.FirstOrDefault(m => m.Id == _gameState.CurrentMapId.CurrentValue);
+            if (currentMap == null)
+            {
+                Debug.LogError($"Couldn't find MapState for id: {_gameState.CurrentMapId.CurrentValue}");
+                return false;
+            }
+            
+            var characterId = command.CharacterId;
+            bool exists = currentMap.Entities.Any(e => e.UniqueId == characterId);
+            if (!exists)
+            {
+                Debug.LogError($"Couldn't find character with id: {characterId}");
+                return false;
+            }
+            
+            _gameState.ControllableEntityId.Value = characterId;
+
+            return true;
+        }
+    }
+}
