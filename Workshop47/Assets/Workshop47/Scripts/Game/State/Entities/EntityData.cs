25ed4304427d317c2376024f6e2a84c405ecbbf1
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 15:53:25 +0300

    Create abstrtact entity

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Entities/EntityData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Workshop47.Scripts.Game.State.Entities
+{
+    public class EntityData
+    {
+        public int UniqueId { get; set; }
+        public string ConfigId { get; set; }
+        public EntityType Type { get; set; }
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+    }
+}
