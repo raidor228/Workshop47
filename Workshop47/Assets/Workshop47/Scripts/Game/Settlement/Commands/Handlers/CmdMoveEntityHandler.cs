7562942cd48ccbb3709ac1efaf303a0bb539ed72
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 20:17:56 +0300

    Create move entity command

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/Handlers/CmdMoveEntityHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using UnityEngine;
+using Workshop47.Scripts.Game.State.Commands;
+using Workshop47.Scripts.Game.State.Root;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands.Handlers
+{
+    public class CmdMoveEntityHandler : ICommandHandler<CmdMoveEntity>
+    {
+        private readonly GameState _gameState;
+
+        public CmdMoveEntityHandler(GameState gameState)
+        {
+            _gameState = gameState;
+        }
+        
+        public bool Handle(CmdMoveEntity command)
+        {
+            var currentMap = _gameState.Maps.FirstOrDefault(m => m.Id == _gameState.CurrentMapId.CurrentValue);
+            if (currentMap == null)
+            {
+                Debug.LogError($"Couldn't find MapState for id: {_gameState.CurrentMapId.CurrentValue}");
+                return false;
+            }
+
+            var entityId = command.EntityId;
+            var entity = currentMap.Entities.First(e => e.UniqueId == entityId);
+            entity.Position.Value = command.Position;
+
+            return true;
+        }
+    }
+}
