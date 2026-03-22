25503070300320725db7a6491218a4cd9fc50e40
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 17:38:40 +0300

    Add maps to game settings

--- Workshop47/Assets/Workshop47/Scripts/Game/Settings/GameSettings.cs
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/GameSettings.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using Workshop47.Scripts.Game.Settings.Settlement.Entities;
+using Workshop47.Scripts.Game.Settings.Settlement.Maps;
 
 namespace Workshop47.Scripts.Game.Settings
 {
@@ -7,5 +8,6 @@ namespace Workshop47.Scripts.Game.Settings
     public class GameSettings : ScriptableObject
     {
         public EntitiesSettings EntitiesSettings;
+        public MapsSettings MapsSettings;
     }
 }
