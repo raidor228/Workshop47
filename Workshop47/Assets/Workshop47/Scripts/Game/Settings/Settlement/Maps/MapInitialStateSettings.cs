cc8b329082bc8981f44083221911eecd9b3497ca
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 17:36:39 +0300

    Create maps settings

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/Settlement/Maps/MapInitialStateSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Workshop47.Scripts.Game.Settings.Settlement.Entities;
+
+namespace Workshop47.Scripts.Game.Settings.Settlement.Maps
+{
+    [Serializable]
+    public class MapInitialStateSettings
+    {
+        public List<EntityInitialStateSettings> Entities;
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settings/Settlement/Maps/MapSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Workshop47.Scripts.Game.Settings.Settlement.Maps
+{
+    [CreateAssetMenu(fileName = "MapSettings", menuName = "Game Settings/Maps/New Map Settings")]
+    public class MapSettings : ScriptableObject
+    {
+        public int MapId;
+        public MapInitialStateSettings InitialStateSettings;
+    }
+}
