cd6e9c796655eb744784e9a761c207bdf43956fa
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 16:20:42 +0300

    Create entities data factory

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/Settlement/Commands/Handlers/EntitiesDataFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using Workshop47.Scripts.Game.Settings.Settlement.Entities;
+using Workshop47.Scripts.Game.State.Entities;
+
+namespace Workshop47.Scripts.Game.Settlement.Commands.Handlers
+{
+    public static class EntitiesDataFactory
+    {
+        public static EntityData CreateEntity(EntityInitialStateSettings initialSettings)
+        {
+            switch (initialSettings.EntityType)
+            {
+                default:
+                    throw new Exception($"Not implemented entity creation: {initialSettings.EntityType}");
+            }
+        }
+        
+        private static T CreateEntity<T>(EntityInitialStateSettings initialSettings) where T : EntityData, new()
+        {
+            return CreateEntity<T>(
+                initialSettings.EntityType,
+                initialSettings.ConfigId,
+                initialSettings.InitialPosition,
+                initialSettings.InitialRotation);
+        }
+
+        public static T CreateEntity<T>(EntityType type, string configId, Vector3 position, Vector3 rotation)
+            where T : EntityData, new()
+        {
+            var entity = new T
+            {
+                Type = type,
+                ConfigId = configId,
+                Position = position,
+                Rotation = rotation
+            };
+
+            switch (entity)
+            {
+                default:
+                    throw new Exception($"Not implemented entity creation: {type}");
+            }
+
+            return entity;
+        }
+    }
+}
