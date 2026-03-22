d1bd616f54b13f77a58a98f4cc02484a45309c52
iamraidor <aleturovichyevgeny@gmail.com>
2026-03-22 16:09:40 +0300

    Create entities factory

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Entities/EntitiesFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Workshop47.Scripts.Game.State.Entities
+{
+    public static class EntitiesFactory
+    {
+        public static Entity CreateEntity(EntityData entityData)
+        {
+            switch (entityData.Type)
+            {
+                default:
+                    throw new Exception("Unsupported entity type: " + entityData.Type);
+            }
+        }
+    }
+}
