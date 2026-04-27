70aef15f038faa115f6677052cbfe08ee8705071
iamraidor <aleturovichyevgeny@gmail.com>
2026-04-27 16:47:48 +0300

    Create chunk state

---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Chunks/BlockType.cs
@@ -0,0 +1,13 @@
+﻿namespace Workshop47.Scripts.Game.State.Chunks
+{
+    public enum BlockType
+    {
+        Air,
+        Grass,
+        Dirt,
+        Stone,
+        SnowBlock,
+        WhiteConcrete,
+        Glass,
+    }
+}
---
+++ Workshop47/Assets/Workshop47/Scripts/Game/State/Chunks/ChunkData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Workshop47.Scripts.Game.State.Chunks
+{
+    public class ChunkData
+    {
+        public const int ChunkWidth = 16;
+        public const int ChunkWidthSq = ChunkWidth * ChunkWidth;
+        public const int ChunkHeight = 128;
+        
+        public Vector2Int Position { get; set; }
+        public BlockType[] Blocks { get; set; }
+
+        public BlockType GetBlock(int x, int y, int z)
+        {
+            int index = x + y * ChunkWidthSq + z * ChunkWidth;
+            return Blocks[index];
+        }
+    }
+}
